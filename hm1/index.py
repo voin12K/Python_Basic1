8b65a4d632627d95d77bf6016595dd2d88a3049f
voin12K <vladleurda@gmail.com>
2024-03-26 10:58:16 +0100

    first commit

--- hm1/index.py
+++ hm1/index.py
@@ -1,1 +1,9 @@
-print("121")
+input_string = input("Введіть довільну строку: ")
+
+even_chars = input_string[::2]
+
+reverse_upper = input_string[::-1].upper()
+
+print("Введена строка:", input_string)
+print("Строка з парних символів:", even_chars)
+print("Строка, написана у зворотній послідовності та у верхньому регістрі:", reverse_upper)
